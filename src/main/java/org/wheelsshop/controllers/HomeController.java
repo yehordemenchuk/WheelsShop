13bab6aa84e247debfed0dd806f66840c14a53f1
Yehor Demenchuk <demenchuk1210m@gmail.com>
2025-04-26 23:08:46 +0200

    fix bugs

--- src/main/java/org/wheelsshop/controllers/HomeController.java
+++ src/main/java/org/wheelsshop/controllers/HomeController.java
@@ -1,9 +1,9 @@
 package org.wheelsshop.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/api/v1/home")
     public String homeController() {
